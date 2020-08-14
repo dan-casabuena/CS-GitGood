392a1bbdb32bf0b38cecc4a8ab2322691718bd21
dan-casabuena <daniel.casabuena25@gmail.com>
2020-08-13 21:14:23 -0400

    Added AvgDistance & Aces

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ players_raw = parsed_html.findAll('div', {"class":"playerbadge ng-scope"})
 
 with open('players.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['player_name', 'KillDeathRatio', 'HeadshotRatio'])
+    writer.writerow(['player_name', 'HeadshotRatio', 'AvgDistance', 'Aces', 'KillDeathRatio'])
     for player in players_raw:
         nick = player.findAll('div', {"class":"name"})[0].contents[1].text
         stats = player.findAll('div', {"class":"value ng-binding"})
         kdr = stats[0].text
         hs = float(stats[1].text[:-1]) / 100
-        writer.writerow([nick, hs, kdr])
+        distance = stats[3].text
+        aces = stats[4].text
+        writer.writerow([nick, hs, distance, aces, kdr])
